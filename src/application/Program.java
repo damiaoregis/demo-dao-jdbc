a397ce71e67b2b9cae1da662106b2dfd1a633150
damiaoregis <damiao.v.serrana@gmail.com>
2026-03-04 15:29:59 -0300

    FindById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,17 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1,"Books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(23, "Jo", "jo@gmail.com",new Date(),3000.0,obj);
-		System.out.println(seller);
+		Seller s = sellerDao.findById(3);
+		
+		System.out.println(s);
 
 	}
 
--- src/db/DB.java
+++ src/db/DB.java
@@ -25,7 +25,7 @@ public class DB{
     }
 
     private static Properties loadProperties() {
-        try (FileInputStream fs = new FileInputStream("db.properties")) {
+        try (FileInputStream fs = new FileInputStream("C:\\Users\\damia\\eclipse-workspace\\demo_dao_jdbc\\src\\db.properties")) {
             Properties props = new Properties();
             props.load(fs);
             return props;
